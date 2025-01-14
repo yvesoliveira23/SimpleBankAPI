3fedb9476a5c0fa832b24b3416fc7425b7de9118
yvesoliveira23 <YVES.OLIVEIRA@ICLOUD.COM>
2025-01-14 10:30:44 -0300

    Update database configuration and migrations to include CreateUser and CreateTransaction

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,18 +10,17 @@ public func configure(_ app: Application) async throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    ), as: .psql))
 
-    app.migrations.add(CreateTodo())
+    app.migrations.add(CreateUser())
+    app.migrations.add(CreateTransaction())
 
     app.views.use(.leaf)
-
 
     // register routes
     try routes(app)
